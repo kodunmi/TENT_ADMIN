3439367868ca3bc94dc80434f97b5f9e8a7738fe
kodunmi <53086177+kodunmi@users.noreply.github.com>
2024-03-27 07:30:16 +0100

    added new base

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,10 +1,12 @@
 import process from "process";
 
-const development: boolean = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
+const development: boolean =
+  !process.env.NODE_ENV || process.env.NODE_ENV === "development";
 
-export default function isDev(): boolean
-{
-    return development;
+export default function isDev(): boolean {
+  return development;
 }
 
-export const BaseUrl = isDev ? "https://tent-group-server.herokuapp.com/api/v1/" : "https://tent-group-server.herokuapp.com/api/v1"
+export const BaseUrl = isDev
+  ? "https://tent-group-server.onrender.com/api/v1"
+  : "https://tent-group-server.onrender.com/api/v1";
